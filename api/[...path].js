5f28920ab09faa332b6285f4cfbafb841a99577e
kaizenmidias <kaizenmidias@users.noreply.github.com>
2026-06-20 15:33:22 -0300

    fix: normalize vercel api request paths

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,9 @@
 const app = require('../server/server');
 
 module.exports = async function handler(req, res) {
+  if (typeof req.url === 'string' && !req.url.startsWith('/api')) {
+    req.url = `/api${req.url.startsWith('/') ? '' : '/'}${req.url}`;
+  }
+
   return app(req, res);
 };
